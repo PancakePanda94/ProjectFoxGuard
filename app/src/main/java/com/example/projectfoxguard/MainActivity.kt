0879bbd9f0d3fe2847ebce7c253364a8d49a1738
Paul Orozco <paul.orozco@cetysuniversidad.edu.mx>
2024-05-06 01:41:40 -0700

    Scanner de codigos QR

--- app/src/main/java/com/example/projectfoxguard/MainActivity.kt
+++ app/src/main/java/com/example/projectfoxguard/MainActivity.kt
@@ -1,46 +1,94 @@
 package com.example.projectfoxguard
 
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.projectfoxguard.ui.theme.ProjectFoxGuardTheme
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.projectfoxguard.databinding.ActivityMainBinding
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
+import com.journeyapps.barcodescanner.ScanOptions
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            ProjectFoxGuardTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+class MainActivity : AppCompatActivity() {
+    private val requestPermissionLauncher=
+        registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            isGranted: Boolean ->
+            if(isGranted){
+                showCamera()
+            }else{
+
+            }
+        }
+
+    private val scanLauncher =
+        registerForActivityResult(ScanContract()){
+            result: ScanIntentResult ->
+            run {
+                if (result.contents == null) {
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show()
+                } else {
+                    setResult(result.contents)
                 }
             }
         }
+
+    private lateinit var binding: ActivityMainBinding
+
+    private fun setResult(string: String){
+        binding.textResult.text = string
+
     }
-}
+    private fun showCamera() {
+        val options = ScanOptions()
+        options.setDesiredBarcodeFormats(ScanOptions.QR_CODE)
+        options.setPrompt("Scan QR Code")
+        options.setCameraId(0)
+        options.setBeepEnabled(false)
+        options.setBarcodeImageEnabled(true)
+        options.setOrientationLocked(false)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        scanLauncher.launch(options)
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ProjectFoxGuardTheme {
-        Greeting("Android")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        initBinding()
+        initViews()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun initViews() {
+        binding.fab.setOnClickListener{
+            checkPermissionCamera(this)
+        }
+    }
+
+    private fun checkPermissionCamera(context: Context) {
+        if(ContextCompat.checkSelfPermission(context,android.Manifest.permission.CAMERA)==PackageManager.PERMISSION_GRANTED) {
+            showCamera()
+        }
+        else if(shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA)){
+            Toast.makeText(context,"Acceso a Camara requerido", Toast.LENGTH_SHORT).show()
+        }
+        else{
+
+            requestPermissionLauncher.launch(android.Manifest.permission.CAMERA)
+        }
+    }
+
+    private fun initBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
